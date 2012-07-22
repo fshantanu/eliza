617a122467f14819380b17a067b982089199bc4f
fshantanu <f.shantanu@gmail.com>
2012-07-22 13:45:47 -0400

    formatting

--- src/main/java/eloquent/eliza/processor/BirthdayProcessor.java
+++ src/main/java/eloquent/eliza/processor/BirthdayProcessor.java
@@ -16,12 +16,11 @@ public class BirthdayProcessor implements Runnable {
 	 * Facebook to process birthday events
 	 */
 	private FacebookHandler facebookHandler;
-	
+
 	/**
 	 * Logger for logging
 	 */
 	private Logger logger = Logger.getLogger(getClass());
-
 
 	@Override
 	public void run() {
@@ -31,34 +30,35 @@ public class BirthdayProcessor implements Runnable {
 		logger.info("Processing Birthday events for - " + today);
 
 		// Get the user object for the logged in user
-		User me = facebookHandler.getObject(facebookHandler.getUserId(), User.class);
-		// get a list of friends for the user. 
+		User me = facebookHandler
+				.getObject(facebookHandler.getUserId(), User.class);
+		// get a list of friends for the user.
 		Collection<User> friends = facebookHandler.getFriendList();
 
 		// for each user check if his birthday today.
 		// if so wish the user Happy Birthday!
-		for(User friend: friends){
+		for (User friend : friends) {
 			User user = facebookHandler.getObject(friend.getId(), User.class);
-			if(user.getBirthday()!=null && 
-					formatter.format(user.getBirthday()).equals(today)){
+			if (user.getBirthday() != null
+					&& formatter.format(user.getBirthday()).equals(today)) {
 
-				try{
-					logger.info(String.format("Wishing %s happy birthday", user.getName()));
+				try {
+					logger
+					.info(String.format("Wishing %s happy birthday", user.getName()));
 					Post post = new Post();
 					post.setFrom(me);
-					post.setMessage(String.format("Happy Birthday %s !", user.getFirstName()));
+					post.setMessage(String.format("Happy Birthday %s !",
+							user.getFirstName()));
 					facebookHandler.postOnWall(post, user);
-				}
-				catch(Throwable ex){
-					logger.error(String.format("Error processing birthday event " +
-							"for user [%s]", user.getName()), ex);
+				} catch (Throwable ex) {
+					logger.error(String.format("Error processing birthday event "
+							+ "for user [%s]", user.getName()), ex);
 				}
 			}
 		}
 
 		logger.info("Finished Processing birthday events");
 	}
-
 
 	public void setFacebookHandler(FacebookHandler facebookHandler) {
 		this.facebookHandler = facebookHandler;
--- src/main/java/eloquent/eliza/processor/PostProcessor.java
+++ src/main/java/eloquent/eliza/processor/PostProcessor.java
@@ -20,16 +20,15 @@ import eloquent.eliza.rest.FacebookHandler;
 public class PostProcessor implements Runnable {
 
 	/**
-	 * logger for logging 
+	 * logger for logging
 	 */
 	private Logger logger = Logger.getLogger(getClass());
 
-	public PostProcessor(){
-		try{
+	public PostProcessor() {
+		try {
 			ClassPathResource resource = new ClassPathResource("ProcessedPosts.txt");
-			file = resource.getFile(); 
-		}
-		catch(Throwable ex){
+			file = resource.getFile();
+		} catch (Throwable ex) {
 			throw new RuntimeException(ex);
 		}
 	}
@@ -52,44 +51,44 @@ public class PostProcessor implements Runnable {
 	public void run() {
 
 		logger.info("Starting processing posts");
-		
+
 		// load the list of processed posts
 		List<String> processedEntities = readFromFile();
-		if(processedEntities == null)
+		if (processedEntities == null)
 			processedEntities = new ArrayList<String>();
 
 		List<String> newEntities = new ArrayList<String>();
 
 		// Read feed from the user's wall
 		Collection<Post> feed = facebookHandler.getFeed();
-		for(Post post: feed){
+		for (Post post : feed) {
 
-			try{
+			try {
 				// process the message in the post
-				String postReply = processEntity(post.getId(), 
-						post.getMessage(), post.getFrom(), processedEntities);
-				if(postReply!=null){
+				String postReply = processEntity(post.getId(), post.getMessage(),
+						post.getFrom(), processedEntities);
+				if (postReply != null) {
 					facebookHandler.commentOnPost(createComment(post, postReply));
 					newEntities.add(post.getId());
 				}
 
 				// Get a list of comments for the post
 				Collection<Comment> comments = facebookHandler.getComments(post);
-				for(Comment comment: comments){
+				for (Comment comment : comments) {
 					// process the message in the comment
-					String commentReply = processEntity(comment.getId(), 
+					String commentReply = processEntity(comment.getId(),
 							comment.getMessage(), comment.getFrom(), processedEntities);
-					if(commentReply!=null){
-						String personalisedReply =  
-								String.format("@%s: %s", comment.getFrom().getName(), commentReply); 
-						facebookHandler.commentOnPost(createComment(post, personalisedReply));
+					if (commentReply != null) {
+						String personalisedReply = String.format("@%s: %s", comment
+								.getFrom().getName(), commentReply);
+						facebookHandler
+								.commentOnPost(createComment(post, personalisedReply));
 						newEntities.add(comment.getId());
 					}
 				}
-			}
-			catch(Exception ex){
-				logger.error(String.format("Error processing post [%s]. The post will" +
-						"be processed in the next iteration.", post.getMessage()), ex);
+			} catch (Exception ex) {
+				logger.error(String.format("Error processing post [%s]. The post will"
+						+ "be processed in the next iteration.", post.getMessage()), ex);
 			}
 		}
 		// write the newly processed entries to the file
@@ -97,68 +96,67 @@ public class PostProcessor implements Runnable {
 		logger.info("finished processing all posts");
 	}
 
-
-	private Comment createComment(Post post, String message){
+	private Comment createComment(Post post, String message) {
 		Comment comment = new Comment();
 		comment.setPost(post);
 		comment.setMessage(message);
 		return comment;
 	}
 
-	private String processEntity(String id, String message, 
-			User from, Collection<String> processedItems){
+	private String processEntity(String id, String message, User from,
+			Collection<String> processedItems) {
 
 		// This is the id for eloquent.eliza
-		if(message== null || processedItems.contains(id) || from.getId().equals("100002508051477"))
+		if (message == null || processedItems.contains(id)
+				|| from.getId().equals("100002508051477"))
 			return null;
 
-		return eliza.getReply(message);	
+		return eliza.getReply(message);
 	}
 
-	private void writeToFile(Collection<String> list){
+	private void writeToFile(Collection<String> list) {
 		FileWriter writer;
 		try {
 
 			writer = new FileWriter(file, true);
-			for(String id: list){
-				writer.write( id + "\n" );
+			for (String id : list) {
+				writer.write(id + "\n");
 			}
 			writer.close();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private List<String> readFromFile(){
+	private List<String> readFromFile() {
 
 		List<String> list = new ArrayList<String>();
-		try{
+		try {
 
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String str;
-			while((str = reader.readLine())!=null){
+			while ((str = reader.readLine()) != null) {
 				list.add(str);
 			}
 			reader.close();
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 		return list;
 	}
-	
+
 	/**
 	 * Set Eliza
+	 * 
 	 * @param eliza
 	 */
 	public void setEliza(Eliza eliza) {
 		this.eliza = eliza;
 	}
 
-
 	/**
 	 * Facebook handler
+	 * 
 	 * @param facebookHandler
 	 */
 	public void setFacebookHandler(FacebookHandler facebookHandler) {
